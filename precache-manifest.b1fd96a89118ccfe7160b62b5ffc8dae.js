aa51750707f050a413348ef9ec0267baf5abc59b
Your Name <cristiradu1605@yahoo.com>
2020-02-12 15:29:28 +0200

    Updates

---
+++ precache-manifest.b1fd96a89118ccfe7160b62b5ffc8dae.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a0c200701e29b93a05c00cd2965edbbb",
+    "url": "/faceDetection/index.html"
+  },
+  {
+    "revision": "0fd4088aeb4e9f6c009d",
+    "url": "/faceDetection/static/css/2.eb45ad44.chunk.css"
+  },
+  {
+    "revision": "71fd0dc68da1c8d8e082",
+    "url": "/faceDetection/static/css/main.95a8485c.chunk.css"
+  },
+  {
+    "revision": "0fd4088aeb4e9f6c009d",
+    "url": "/faceDetection/static/js/2.899962f3.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/faceDetection/static/js/2.899962f3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "71fd0dc68da1c8d8e082",
+    "url": "/faceDetection/static/js/main.d13692dc.chunk.js"
+  },
+  {
+    "revision": "9a3f4341d1b1899fe910",
+    "url": "/faceDetection/static/js/runtime-main.6b1e64c7.js"
+  },
+  {
+    "revision": "9fda4841a8db52b7b3d81f9f7f949ca1",
+    "url": "/faceDetection/static/media/logo.9fda4841.png"
+  }
+]);
